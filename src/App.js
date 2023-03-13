23fa40ed48f0a5c42e275aed6d2b657d8a8b3899
trabelsi1302 <elyestrfr@yahoo.fr>
2023-03-13 16:24:42 +0100

    sacem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Button from "react-bootstrap/Button";
+
+import "./App.css";
+function App1(){
+  return(
+    <div id='sacem'>
+      <img src="logo512.png" class="rounded float-left" alt="logo"></img> 
+    </div> 
+  );
+}
+
 function App() {
   return (
-    <div className="App">
+   <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id='sacem'>
+      <img src="logo.png" class="rounded float-left" alt="logo"></img> 
+    </div>       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Sacem group
         </p>
         <a
           className="App-link"
@@ -17,9 +34,44 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+        Elyes 
+        Elyes 
+        Elyes 
+
+        </p>
+        <div>
+        <form>
+        <div class="form-group">
+    <label for="Firstname">First Name</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="firstname"></input>
+  </div>
+  <div class="form-group">
+    <label for="Lastname">First Name</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="lastname"></input>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+  </div>
+  
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+       
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Button } from 'react-bootstrap'
+
+import React from 'react';
+function Header() {
+    return(
+        <nav class="bg-dark navbar-dark navbar">
+            <div className="row col-12 d-flex justify-content-center text-white">
+                <h3>Registration</h3>
+            </div>
+        </nav>
+    )
+}
+export default Form;
